5c51a42654aded7d63e122c2b277b1a1bbfb3633
guotianji <guotianji@guotianjideiMac.local>
2015-02-26 17:38:50 +0800

    cocoapods
    导入reactivecocoa

--- lyric/AppDelegate.m
+++ lyric/AppDelegate.m
@@ -8,8 +8,13 @@
 
 #import "AppDelegate.h"
 #import "Macro.h"
+#import "KMCGeigerCounter/KMCGeigerCounter.h"
+#import "ReactiveCocoa.h"
 
 @interface AppDelegate ()
+
+@property (nonatomic, strong) NSString *username;
+@property (nonatomic, strong) UIButton *button;
 
 @end
 
@@ -19,7 +24,28 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    RACSubject *letters = [RACSubject subject];
+    RACSubject *numbers = [RACSubject subject];
+    RACSignal *signalOfSignals = [RACSignal createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+        [subscriber sendNext:letters];
+        [subscriber sendNext:numbers];
+        [subscriber sendCompleted];
+        return nil;
+    }];
     
+    RACSignal *flattened = [signalOfSignals flatten];
+    
+    // Outputs: A 1 B C 2
+    [flattened subscribeNext:^(NSString *x) {
+        NSLog(@"%@", x);
+    }];
+    
+    [letters sendNext:@"A"];
+    [numbers sendNext:@"1"];
+    [letters sendNext:@"B"];
+    [letters sendNext:@"C"];
+    [numbers sendNext:@"2"];
+
     
     return YES;
 }
@@ -27,6 +53,8 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    [KMCGeigerCounter sharedGeigerCounter].enabled = NO;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -40,6 +68,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    [KMCGeigerCounter sharedGeigerCounter].enabled = YES;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
